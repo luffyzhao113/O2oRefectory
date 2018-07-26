c77b7264d1d664b06abe656926fc930df22a8c81
“luffyzhao <luffyzhao@vip.126.com>
2018-07-26 20:36:24 +0800

    初始化 数据更新

--- database/backup/base_logs.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `base_logs` VALUES (1, 'base/auth', '{\"email\":\"luffyzhao@vip.126.com\",\"password\":\"123456\"}', 1, 200, '2018-7-24 22:41:08', '2018-7-24 22:41:08', 'POST', '用户登录');
-INSERT INTO `base_logs` VALUES (2, 'base/auth', '{\"email\":\"luffyzhao@vip.126.com\",\"password\":\"123456\"}', 1, 200, '2018-7-24 23:12:25', '2018-7-24 23:12:25', 'POST', '用户登录');
--- database/backup/seller_certificates.sql
+++ database/backup/seller_certificates.sql
@@ -1,1 +1,1 @@
-INSERT INTO `seller_certificates` VALUES (1, 1, '杨家辉', '2018-7-24 23:13:05', '2018-7-24 23:13:08');
+INSERT INTO `seller_certificates` VALUES (1, 1, '杨家辉', 0,'2018-7-24 23:13:05', '2018-7-24 23:13:08');
