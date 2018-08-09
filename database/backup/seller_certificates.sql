b6564de64e98f88fa5275af9c0cd0b2d8bf7e1ff
“luffyzhao <luffyzhao@vip.126.com>
2018-08-10 07:48:31 +0800

    数据备份

--- database/backup/seller_certificates.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `seller_certificates` VALUES (1, 1, '杨家辉', 0,'2018-7-24 23:13:05', '2018-7-24 23:13:08');
---
+++ database/backup/seller_materials.sql
@@ -0,0 +1,1 @@
+INSERT INTO `seller_materials` (`id`, `seller_id`, `name`, `status`, `created_at`, `updated_at`) VALUES (1, 1, '杨家辉', 0, '2018-7-24 23:13:05', '2018-7-24 23:13:08');
