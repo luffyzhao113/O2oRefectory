e8f9a38bcd72e5f2d7f06e7b4a931cdf80921b79
luffyzhao <luffyzhao@vip.126.com>
2018-10-10 18:01:12 +0800

    商品和商品分类

---
+++ database/migrations/2018_10_10_154353_create_base_categorys_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBaseCategorysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'base_categorys',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->unsignedInteger('parent_id')->comment("父id");
+                $table->string('name', 50)->comment('分类名称');
+                $table->smallInteger('sort')->default(255)->comment('排序');
+                $table->timestamps();
+            }
+        );
+
+        Schema::create(
+            'base_attributes',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name', 20)->comment('属性名');
+                $table->unsignedInteger('category_id')->comment('分类ID');
+            }
+        );
+
+        Schema::create(
+            'base_attribute_values',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name', 20)->comment('属性值');
+                $table->unsignedInteger('attribute_id')->comment('属性ID');
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('base_categorys');
+        Schema::dropIfExists('base_attributes');
+        Schema::dropIfExists('base_attribute_values');
+    }
+}
---
+++ database/migrations/2018_10_10_161117_create_seller_goods_table.php
@@ -0,0 +1,107 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSellerGoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'seller_goodss',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->unsignedInteger('seller_id')->comment('店铺ID');
+                $table->unsignedInteger('category_id')->comment('分类id');
+                $table->string('bar_code', 32)->comment('商品条码');
+                $table->string('name', 50)->comment('商品名称');
+                $table->string('image', 100)->comment('商品图片');
+                $table->unsignedInteger('price')->comment('商品价格：单位分');
+                $table->unsignedInteger('stock')->comment('库存');
+                $table->unsignedInteger('sale')->comment('销量');
+                $table->tinyInteger('status', false, true)->comment('状态： 0:未审核 1:已审核 2:禁售');
+                $table->tinyInteger('is_sale', false, true)->comment('上架状态：0:不上架 1:上架');
+                $table->string('specs', 255)->comment('商品规格:[{"名称"：["值"]}]');
+                $table->string('attribute', 255)->comment('商品店铺私有属性：["属性名:属性值"]');
+                $table->string('base_attribute', 255)->comment('商品平台属性：属性名:属性值,属性名:属性值  利用 find_in_set 进行搜索');
+                $table->timestamps();
+
+                $table->index(['seller_id', 'status', 'is_sale']);
+                $table->index(['status', 'is_sale', 'category_id']);
+            }
+        );
+
+        Schema::create('seller_goods_details', function (Blueprint $table){
+            $table->unsignedInteger('goods_id')->comment('外键：商品表');
+            $table->text('content')->comment('商品详情表');
+
+            $table->primary('goods_id');
+        });
+
+        Schema::create('seller_goods_gallerys', function (Blueprint $table){
+            $table->increments('id');
+            $table->unsignedInteger('goods_id')->comment('外键：商品表');
+            $table->unsignedInteger('seller_id')->comment('店铺ID');
+            $table->string('path')->comment('地址');
+            $table->string('ext')->comment('文件类型');
+            $table->tinyInteger('sort')->default(255)->comment('排序');
+            $table->timestamps();
+
+            $table->index('goods_id');
+            $table->index('seller_id');
+        });
+
+        Schema::create(
+            'seller_specs',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->unsignedInteger('seller_id')->comment('店铺ID');
+                $table->string('name', 20)->comment('规格名称');
+                $table->timestamps();
+
+                $table->index('seller_id');
+            }
+        );
+
+
+        Schema::create(
+            'seller_spec_values',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name', 20)->comment('规格值');
+                $table->unsignedInteger('spec_id')->comment('规格ID');
+            }
+        );
+
+        Schema::create(
+            'seller_goods_skus',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->unsignedInteger('goods_id')->comment('商品ID');
+                $table->string('specs', 255)->comment('商品SKU规格： [{"名称"："值"}]');
+                $table->unsignedInteger('price')->comment('商品价格：单位分');
+                $table->unsignedInteger('stock')->comment('库存');
+
+                $table->index('goods_id');
+            }
+        );
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seller_goods');
+    }
+}
